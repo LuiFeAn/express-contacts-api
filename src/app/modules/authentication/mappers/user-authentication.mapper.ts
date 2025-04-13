c43041008748da42b0d21f4e8f3840fc3bec0685
LuiFeAn <luisfernandogvv@gmail.com>
2025-04-13 11:44:29 -0300

    refactor: update user authentication mapper to return UserAuthenticationResponse

--- src/app/modules/authentication/mappers/user-authentication.mapper.ts
+++ src/app/modules/authentication/mappers/user-authentication.mapper.ts
@@ -1,9 +1,9 @@
 import { User } from "../../user/model";
-
+import { UserAuthenticationResponse } from "../responses/user-authentication.response";
 export class UserAuthenticationMapper {
-  static toRequest(user: User) {
+  static toResponse(user: User): UserAuthenticationResponse {
     return {
-      id: user.id,
+      id: user.id as number,
       name: user.name,
       email: user.email,
     };
---
+++ src/app/modules/authentication/responses/user-authentication.response.ts
@@ -0,0 +1,5 @@
+export interface UserAuthenticationResponse {
+  id: number;
+  name: string;
+  email: string;
+}
