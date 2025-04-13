7c1eeb0a17c445ee17abb4c6f653d098ef054e5d
LuiFeAn <luisfernandogvv@gmail.com>
2025-04-13 10:40:44 -0300

    feat: add findByEmail method to IUserRepository interface for user retrieval by email

--- src/app/modules/user/repositories/user.interface.repository.ts
+++ src/app/modules/user/repositories/user.interface.repository.ts
@@ -2,5 +2,6 @@ import { User } from "../model";
 
 export interface IUserRepository {
   create(user: User): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
   emailExists(email: string): Promise<boolean>;
 }
